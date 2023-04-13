e0aaf3ddde37efef996bb7ea0a937cdbab127420
stctai <84058750+stctai@users.noreply.github.com>
2023-04-12 20:56:29 -0700

    Refactored controllers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -37,15 +37,43 @@ module.exports = (app) => {
             });
     }
 
+    const findCurrentUser = (req, res) => {
+        const currentUser = req.session['currentUser'];
+        if (currentUser) {
+            res.json(currentUser);
+        } else {
+            res.sendStatus(204);
+        }
+    }
+
+    const findUserById = (req, res) => {
+        const targetId = req.params['uid'];
+        userService.findUserById(targetId)
+            .then((user) => {
+                res.send(user);
+            })
+            .catch(err => console.error(`findUserById error: ${err}`));
+    }
+
+    const updateUser = (req, res) => {
+        const updatedUser = req.body;
+        userService.updateUser(updatedUser)
+            .then((newUser) => {
+                res.json(newUser);
+            });
+    }
+
     const deleteUserById = (req, res) => {
         const id = req.body._id;
         userService.deleteUser(id)
             .then(user => res.json(user));
     }
 
-
     app.post("/api/register", register);
     app.post("/api/login", login);
     app.get("/api/users", findAllUsers);
+    app.get("/api/user", findCurrentUser);
+    app.get("/api/users/:uid", findUserById);
+    app.put("/api/users/:uid", updateUser);
     app.delete("/api/users", deleteUserById);
 };
