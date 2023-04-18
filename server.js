c9ec4a54da58576d164e5a76ee1f13f4ff019fc4
Martin Wang <shenquan.wang@sjsu.edu>
2023-04-18 09:54:59 -0700

    header '*' change to req.headers.origin for resolve error

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
